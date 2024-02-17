ad60ce4549b16ef81fb1056b6d9f5e14dcd86d3c
xaviourprakash <71564900+xaviourprakash@users.noreply.github.com>
2024-02-18 00:37:39 +0530

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./component/MarketingApp";
+
+export default () => {
+	return (
+		<div>
+			<h1>Hi there!</h1>
+			<hr />
+			<MarketingApp />
+		</div>
+	);
+};
